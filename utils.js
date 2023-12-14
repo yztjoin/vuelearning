a2528bd4621105fd0e9b7326f09bb9e8c886c235
seeyou <240192231@qq.com>
2023-12-14 16:23:22 +0800

    新增文档

--- utils.js
+++ utils.js
@@ -1,6 +1,26 @@
-export function foo(obj) {
-  obj && obj.foo
+let miaoMiao = {
+  _name: '疫苗',
+  get name() {
+    return this._name
+  },
 }
-export function bar(obj) {
-  obj && obj.bar
+let miaoXy = new Proxy(miaoMiao, {
+  get(target, prop, receiver) {
+    console.log(target,prop)
+    return Reflect.get(target,prop,receiver)
+  },
+  set(target, prop, newValue,receiver){
+    console.log('++++++++++',target, prop, newValue,receiver)
+    Reflect.set(target,prop,newValue,receiver)
+    return true
+  }
+})
+let kexingMiao = {
+  __proto__: miaoXy,
+  _name: '科兴疫苗',
 }
+console.log(kexingMiao.name)
+kexingMiao._name = "9999"
+console.log(kexingMiao)
+console.log(miaoXy)
+console.log(kexingMiao.name)
